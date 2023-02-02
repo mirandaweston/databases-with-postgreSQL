4862c6aa5e8928c8e6d7fc0a970642906b7e04cc
mirandaweston <mweston105@yahoo.co.uk>
2023-02-02 20:47:19 +0000

    Adding program to print list of books

---
+++ book_store/lib/app.rb
@@ -0,0 +1,16 @@
+# file: app.rb
+
+require_relative 'database_connection'
+require_relative 'book_repository'
+
+# We need to give the database name to the method `connect`.
+DatabaseConnection.connect('book_store')
+
+# Perform a SQL query on the database and get the result set.
+sql = 'SELECT id, title, author_name FROM books'
+result = DatabaseConnection.exec_params(sql, [])
+
+# Print out each record from the result set .
+result.each do |record|
+  puts "#{record['id']} - #{record['title']} - #{record['author_name']}"
+end
